ae2c4b769b6265fd41a35e42cd5f052840ec0d74
Daniel Krah <moorviper@gmail.com>
2018-03-01 03:53:38 +0100

    without math / has to be fixed in docker container

---
+++ code/functions.js
@@ -0,0 +1,11 @@
+globale variable
+
+
+function myFunction (x, y){
+  var z;
+  z = x +y;
+  return z;
+}
+
+
+der Sichtbarkeit / Gültigkeit von Variablen in Java
---
+++ code/javascript.js
@@ -0,0 +1,5 @@
+function suche(){
+  Code-Block
+}
+
+document.getElementsByTagName('body')[0].childKnoten[1].lastChild
